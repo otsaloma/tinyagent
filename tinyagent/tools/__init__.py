ae552f2cc9da20a761f090b82025ec306c90cc78
Osmo Salomaa <otsaloma@iki.fi>
2025-10-19 23:48:51 +0300

    Use relative imports

--- tinyagent/tools/__init__.py
+++ tinyagent/tools/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from tinyagent.tools.web_fetch import WebFetchTool # noqa
-from tinyagent.tools.web_search import WebSearchTool # noqa
+from .web_fetch import WebFetchTool # noqa
+from .web_search import WebSearchTool # noqa
